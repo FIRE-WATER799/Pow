463cca12aa45ef92dacba4f6457c2a410114e727
FIRE-WATER799 <haywo@192.168.1.160>
2021-10-22 14:51:48 -0500

    Added Energy Meter Added More Recipies

--- src/main/java/net/mcreator/powermod/block/SolarPanelBlock.java
+++ src/main/java/net/mcreator/powermod/block/SolarPanelBlock.java
@@ -53,7 +53,7 @@ public class SolarPanelBlock extends PowerModModElements.ModElement {
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(1)
 					.harvestTool(ToolType.PICKAXE).setRequiresTool().notSolid().setOpaque((bs, br, bp) -> false));
 			setRegistryName("solar_panel");
 		}
